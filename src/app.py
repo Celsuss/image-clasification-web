f1b0af7ca05e2227ed8ca77f209d91f24f62dda7
U-LAPTOP-PUIFJ7BU\ronan <ronan.liufy@gmail.com>
2021-01-30 22:06:29 +0100

    some code polish

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, make_response, jsonify
+from flask import Flask, request, make_response, jsonify
+from werkzeug.utils import secure_filename
 import os, time
 from tools.inference import classify
 from flask_cors import CORS
@@ -25,11 +26,11 @@ def address(filename):
 
 @app.route('/')
 def index():
-    return {'Value' : 'Welcome'}
+    return 'Welcome!'
 
 @app.route('/test', methods=['GET'])
 def test():
-    return {'Value' : 'Hello World'}
+    return 'Just a test!'
 
 @app.route('/list_model', methods=['GET'])
 def list_model():
@@ -40,8 +41,7 @@ def list_model():
     
     return make_response(jsonify({"models": supported_models}), 200)
 
-    
-@app.route('/testPost', methods=['POST'])
+@app.route('/post_image', methods=['POST'])
 def testPost():
     f = request.files['file']
     if not os.path.exists(uploadDir):
@@ -49,11 +49,11 @@ def testPost():
     if f:
         filename = f.filename
         if filename.split('.')[-1] in supported_types:
+
+            filename = secure_filename(filename)
             uploadpath = address(filename) 
             f.save(uploadpath) 
 
-            #TODO: get the which model to run prediction
-            # chosen_model = request.body["model"]
             chosen_model = request.form['model_name']
 
             pred, t = classify(uploadpath, chosen_model, models[chosen_model]) 
@@ -70,28 +70,6 @@ def testPost():
 
     return res
 
-# @app.route('/', methods=['POST', 'GET'])
-# def process():
-#     if request.method == 'POST':
-#         f = request.files.get('selectfile') 
-#         if not os.path.exists(uploadDir):
-#             os.makedirs(uploadDir) 
-#         if f:
-#             filename = f.filename
-#             if filename.split('.')[-1] in supported_types:
-#                 uploadpath = address(filename) 
-#                 f.save(uploadpath) 
- 
-#                 pred, t = classify(uploadpath, model)  # classify_with_quantified(uploadpath, interpreter) # classify(uploadpath, model) 
-#                 flash('Upload Load Successful!', 'SUCESS') 
-#                 return render_template('index.html', imagename=filename, predvalue=pred, used_time="{} seconds".format(t)) 
-#             else:
-#                 flash('Unsupported File Types!', 'FAIL')
-#         else:
-#             flash('No File Selected.', 'FAIL')
-
-#     return index()
- 
  
 if __name__ == '__main__':
 
